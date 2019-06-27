59d55996e13526df1f83524b3f1dc4c14a2cdb34
Matthew Di Pasquale <liveloveprosper@gmail.com>
2019-06-27 19:21:41 -0400

    Update Collection.binarySearch(_:) #trivial

--- TokenTextView/Helpers.swift
+++ TokenTextView/Helpers.swift
@@ -26,7 +26,7 @@ extension Collection {
   func binarySearch(_ predicate: (Iterator.Element) -> Bool) -> Index {
     var low = startIndex
     var high = endIndex
-    while low != high {
+    while low < high {
       let mid = index(low, offsetBy: distance(from: low, to: high)/2)
       if predicate(self[mid]) {
         low = index(after: mid)
